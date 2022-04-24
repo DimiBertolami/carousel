fc541a32f3748b0c3fcbeb5cb2c4b8cb1721d056
Dimitri Bertolami <dimibertolami@gmail.com>
2022-04-24 05:41:13 +0200

    template found on bootstrap

---
+++ js/dimi.js
@@ -0,0 +1,102 @@
+function removeLine($elementId) {
+    if ($elementId = "card8Line"){
+        let $Item = document.getElementById("card8Line");
+        $Item.hidden = true;
+        console.log("removing line from card 8")
+        return
+    };
+    if ($elementId = "card7Line"){
+        let $Item = document.getElementById("card7Line");
+        $Item.hidden = true;
+        console.log("removing line from card 7")
+        return;
+    };
+    if ($elementId = "card6Line"){
+        let $Item = document.getElementById("card6Line");
+        $Item.hidden = true;
+        console.log("removing line from card 6")
+        return;
+    };
+    if ($elementId = "card5Line"){
+        let $Item = document.getElementById("card5Line");
+        $Item.hidden = true;
+        console.log("removing line from card 5")
+        return;
+    };
+    if ($elementId = "card4Line"){
+        let $Item = document.getElementById("card4Line");
+        $Item.hidden = true;
+        console.log("removing line from card 4")
+        return;
+    };
+    if ($elementId = "card3Line"){
+        let $Item = document.getElementById("card3Line");
+        $Item.hidden = true;
+        console.log("removing line from card 3")
+        return;
+    };
+    if ($elementId = "card2Line"){
+        let $Item = document.getElementById("card2Line");
+        $Item.hidden = true;
+        console.log("removing line from card 2")
+        return;
+    };
+    if ($elementId = "card1Line"){
+        let $Item = document.getElementById("card1Line");
+        $Item.hidden = true;
+        console.log("removing line from card 1")
+        return;
+    };
+}
+
+function addLine($elementId) {
+    if ($elementId = "card8Line"){
+        let $Item = document.getElementById("card8Line");
+        $Item.hidden = false;
+        console.log("adding line to 8")
+        return;
+    };
+    if ($elementId = "card7Line"){
+        let $Item = document.getElementById("card7Line");
+        $Item.hidden = false;
+        console.log("adding line to 7")
+        return;
+    };
+    if ($elementId = "card6Line"){
+        let $Item = document.getElementById("card6Line");
+        $Item.hidden = false;
+        console.log("adding line to 6")
+        return;
+    };
+    if ($elementId = "card5Line"){
+        let $Item = document.getElementById("card5Line");
+        $Item.hidden = false;
+        console.log("adding line to 5")
+        return;
+    };
+    if ($elementId = "card4Line"){
+        let $Item = document.getElementById("card4Line");
+        $Item.hidden = false;
+        console.log("adding line to 4")
+        return;
+    };
+    if ($elementId = "card3Line"){
+        let $Item = document.getElementById("card3Line");
+        $Item.hidden = false;
+        console.log("adding line to 3")
+        return;
+    };
+    if ($elementId = "card2Line"){
+        let $Item = document.getElementById("card2Line");
+        $Item.hidden = false;
+        console.log("adding line to 2")
+        return;
+    };
+    if ($elementId = "card1Line"){
+        let $Item = document.getElementById("card1Line");
+        $Item.hidden = false;
+        console.log("adding line to 1")
+        return;
+    };
+}
+
